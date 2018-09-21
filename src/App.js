c34015a148d25e13b9959ce9df1298d6d7d623c6
s5935512004 <s5935512004@phuket.psu.ac.th>
2018-09-22 04:08:50 +0700

    Update 5 Add Logo

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import React, { Component } from 'react';
 import './App.css';
 import WordCard from './WordCard';
+import logo from './logo.svg';
 
 const word = "Hello";
 class App extends Component {
   render() {
     return (
-      <div className="AppHandler as Props">
-          <WordCard value="hello"/>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React by 5935512004</h1>
+        </header>
+        <div className="AppHandler as Props">
+          <WordCard value="Hello" />
+        </div>
       </div>
+
     );
   }
 }
